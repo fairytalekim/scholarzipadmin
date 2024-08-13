5f4dc139e5e71d7e217fe48eb8e501384f7abd07
taykim01 <taykim01@gmail.com>
2024-08-13 21:28:37 +0900

    fix: production URL changed

--- src/domain/use_case/url.ts
+++ src/domain/use_case/url.ts
@@ -1,6 +1,6 @@
 const environments = {
     "development": 'http://localhost:3000',
-    "production": 'https://scholarzip-admin.vercel.app/',
+    "production": 'https://scholarzipadmin.vercel.app',
 } as const
 
 const url = environments[process.env.NEXT_PUBLIC_ENVIRONMENT as keyof typeof environments];
